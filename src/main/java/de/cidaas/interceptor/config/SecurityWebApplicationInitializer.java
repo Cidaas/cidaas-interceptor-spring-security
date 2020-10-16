b610dfe5723ba7646e12142eef7f4455d8230ffa
Daniel Storch <daniel.storch@widas.de>
2020-10-16 14:24:48 +0200

    FIx problem race condition problem when this interceptor is used with spring boot

--- src/main/java/de/cidaas/interceptor/config/SecurityWebApplicationInitializer.java
+++ src/main/java/de/cidaas/interceptor/config/SecurityWebApplicationInitializer.java
@@ -1,7 +1,13 @@
 package de.cidaas.interceptor.config;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+//This will ensure that this Initializer will be called before any other 
+//Initializer with the default value Ordered.LOWEST_PRECEDENCE
+//If this is not ensured, spring boot might have a race condition problem when registration the bean springSecurityFilterChain
+//https://stackoverflow.com/questions/28306031/apparent-spring-boot-race-condition-causing-duplicate-springsecurityfilterchain
+@Order(-500)
 public class SecurityWebApplicationInitializer 
   extends AbstractSecurityWebApplicationInitializer {
     // This will simply register the springSecurityFilterChain Filter for every URL in the application
